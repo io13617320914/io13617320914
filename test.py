37c4d76e28989aa8c0cc2d60a2ed0e39ba41903d
io13617320914 <874730590@qq.com>
2022-07-30 17:02:44 +0800

    第一次修改

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-11
+  import os
+  import numpy as np
+  import matplotlib.pyplot as plt
+  from datetime import datetime
+
+-# 如果没有文件夹就创建一个文件夹
+-# sample_dir = 'hoework4.9'
+-# if not os.path.exists(sample_dir)
+-#      os.makedirs(sample_dir)
++# test修改文件
+
+  #增加子文件夹和时间戳
+  now_time = datetime.now().strftime('%Y-%m-%d-%H-%M-%s')
+  son_sample_dir = 'samples//{}',format(now_time)
+  if not os.path.exists(son_sample_dir):
